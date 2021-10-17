be2b3f46aba4652067c2fab57cf7075b17da85f8
sarathchandradamineni <daminenisarathchandra@gmail.com>
2021-10-17 14:06:30 +0200

    My First commit

---
+++ Wish My Friend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wish My Friend
+//
+//  Created by Sarath Chandra Damineni on 2021-10-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
