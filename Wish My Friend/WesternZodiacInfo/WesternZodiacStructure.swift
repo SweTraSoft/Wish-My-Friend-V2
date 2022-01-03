b1c62890378a3804a3c04c5853ed47943d1df2be
sarathchandradamineni <daminenisarathchandra@gmail.com>
2022-01-03 10:16:30 +0100

    Zodiac implementation 1

---
+++ Wish My Friend/WesternZodiacInfo/WesternZodiacStructure.swift
@@ -0,0 +1,19 @@
+//
+//  WesternZodiacStructure.swift
+//  Wish My Friend
+//
+//  Created by Sarath Chandra Damineni on 2021-12-29.
+//
+
+import Foundation
+
+struct WesternZodiacStructure: Codable
+{
+    var id: Int
+    var Name: String
+    var Strengths: String
+    var Weaknesses: String
+    var Likes: String
+    var Dislikes: String
+    var Text: String
+}
